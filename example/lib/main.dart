8968328edc7e6267f0bdba54edff1f32f057a427
pradeepkumar <pradeep.bishnoi89@gmail.com>
2021-08-24 18:37:28 +0530

    updated

---
+++ example/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:example/example_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Rounded Button',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ButtonExample(),
+    );
+  }
+}
